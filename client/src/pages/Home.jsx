e9d6c00109207c7649cd017ade38a4bf89b370a5
Camille Marker <camillemarker@gmail.com>
2023-07-12 11:08:40 -0400

    Welcome/homepage (landing) coded in pages/home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from 'react-router-dom'
+
 const Home = () => {
+  let navigate = useNavigate()
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome</h1>
+      <button onClick={() => navigate('/signin')}>Click Here To Begin</button>
     </div>
   )
 }
