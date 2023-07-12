d83d46d39ce466eb2a6a81d51af2d0981a7b4400
Camille Marker <camillemarker@gmail.com>
2023-07-12 11:11:35 -0400

    Declared useState for register function in pages/Register

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+const Register = () => {
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  })
+}
