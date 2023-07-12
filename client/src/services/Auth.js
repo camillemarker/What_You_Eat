52fcbeb0e35517d52c11229e20053465a612dace
Camille Marker <camillemarker@gmail.com>
2023-07-12 12:00:42 -0400

    RegisterUser function added to Auth.js

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -8,3 +8,12 @@ export const SignInUser = async (data) => {
     throw error
   }
 }
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/auth/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
