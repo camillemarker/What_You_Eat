e3af22da161a455ee814686fc803af1e4576a797
Camille Marker <camillemarker@gmail.com>
2023-07-12 11:04:43 -0400

    Set up Nav bar elements in component

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,1 +1,25 @@
+import { Link } from 'react-router-dom'
 
+const Nav = ({ user, handleLogout }) => {
+  let userOptions
+  if (user) {
+    userOptions = (
+      <nav>
+        <h3>Welcome {user.name}!</h3>
+        <Link onClick={handleLogout} to="/">
+          Sign Out
+        </Link>
+      </nav>
+    )
+  }
+
+  const publicOptions = (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/signin">Sign In</Link>
+    </nav>
+  )
+
+  return <header>{user ? userOptions : publicOptions}</header>
+}
