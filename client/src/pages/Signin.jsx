ef09874c346fd21783587a01d004ea3fbd02f063
Camille Marker <camillemarker@gmail.com>
2023-07-12 11:23:02 -0400

    Added handleChange and handleSubmit functions to SignIn

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,4 +2,12 @@ import { useState } from 'react'
 
 const SignIn = () => {
   const [formValues, setFormValues] = useState({ email: '', password: '' })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
 }
