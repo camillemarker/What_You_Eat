302b8d82872ad0277318fc4ed8d0bd9757b6272b
Camille Marker <camillemarker@gmail.com>
2023-07-11 21:12:06 -0400

    Fixed user model and required user schema in index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const itemSchema = require('./ListItem')
+const userSchema = require('./User')
 
 const Item = mongoose.model('Item', itemSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = {
-  Item
+  Item,
+  User
 }
