4313564a2489e727bd55f8ee43f360d71b31bbe2
Camille Marker <camillemarker@gmail.com>
2023-07-12 10:47:35 -0400

    Set up import/defined route for SignIn and Register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { useState } from 'react'
 import Home from './pages/Home'
+import Register from './pages/Register'
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   )
