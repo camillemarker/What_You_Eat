00a95cb911385b0d40b2b5d6621b923e811f5fed
Camille Marker <camillemarker@gmail.com>
2023-07-13 13:18:08 -0400

    Set up imports/useState for grocerylist, coded fetchItems function for useEffect

--- client/src/pages/GroceryList.jsx
+++ client/src/pages/GroceryList.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const GroceryList = () => {
+  const [items, setitems] = useState([])
+  const [itemName, setItemName] = useState('')
+  const [itemQuantity, setItemQuantity] = useState('')
+
+  useEffect(() => {
+    fetchItems()
+  }, [])
+
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get('/api/items')
+      setitems(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return <div></div>
 }
 
